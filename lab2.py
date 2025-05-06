63fbb6314b9ba2ed601adc24f348e25e7bf88adc
sue-e <suee.24@ichat.sp.edu.sg>
2025-05-06 15:18:33 +0800

    add lab2.py

--- lab2.py
+++ lab2.py
@@ -1,1 +1,40 @@
-print("ET0735 (DEvOps for AIoT) - Lab 2 - Introtion to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67,32)")
+    return
+
+def get_user_input():
+    theinput = str(input())
+    thelist = theinput.split(",")
+    thelist = [float(num.strip()) for num in thelist]
+    return thelist
+
+display_main_menu()
+thelist = get_user_input()
+print(thelist)
+
+def calc_average_temperature(thelist):
+    average = sum(thelist) / len(thelist)
+    print(average)
+    return
+
+def find_min_max(thelist):
+    maxlist = max(thelist)
+    minlist = min(thelist)
+    maxminlist = list[minlist, maxlist]
+    print(maxminlist)
+    return
+
+def sort_list(thelist):
+    sortedlist = sorted(thelist)
+    print(sortedlist)
+    return sortedlist
+
+def median_list(thelist):
+    import statistics
+    medianlist = statistics.median(thelist)
+    print(medianlist)
+    return
+calc_average_temperature(thelist)
+find_min_max(thelist)
+sortedlist = sort_list(thelist)
+median_list(sortedlist)
